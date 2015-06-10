f4245c3a1b1fce21707d814216770119253b0969
mattstyles <matt@veryfizzyjelly.com>
2015-06-10 23:49:04 +0100

    #add first square pass

--- src/components/displacementMap.jsx
+++ src/components/displacementMap.jsx
@@ -1,8 +1,9 @@
 
 import React from 'react'
 import classnames from 'classnames'
+import rnd from 'lodash.random'
 
-import { clamp } from 'utils/maths'
+import { clamp, wrap } from 'utils/maths'
 
 
 export default class DisplacementMap extends React.Component {
@@ -37,17 +38,91 @@ export default class DisplacementMap extends React.Component {
     }
 
     componentDidMount() {
-        console.log( 'mounted' )
-
         this.mounted = true
         this.canvas = React.findDOMNode( this )
 
-        console.log( this.grid )
+        this.generate( 0, 0, this.props.gridSize - 1, this.props.gridSize - 1, 1 )
         this.renderGrid()
     }
 
     componentDidUnmount() {
         this.mounted = false
+    }
+
+
+    /*-----------------------------------------------------------*
+     *
+     *  Generation methods
+     *
+     *-----------------------------------------------------------*/
+
+    random( gen ) {
+        return 0 + ( rnd( -1, 1, true ) * gen )
+    }
+
+    wrap( num ) {
+        return wrap({
+            num: num,
+            min: 0,
+            max: this.props.gridSize
+        })
+    }
+
+    getAverage( x, y, size ) {
+        return ( wrap( x - size ) + wrap( x + size ) + wrap( y - size ) + wrap( y + size ) ) / 4
+    }
+
+    getCell( x, y ) {
+        let cell = this.grid[ this.wrap( x ) ][ this.wrap( y ) ]
+
+        return cell || 0
+    }
+
+    setCell( x, y, value ) {
+        this.grid[ this.wrap( x ) ][ this.wrap( y ) ] = value
+    }
+
+    generate( x1, y1, x2, y2, gen ) {
+        console.log( 'generation:', gen )
+        this.generateSquare( x1, y1, x2, y2, 1 / gen )
+        this.recurse( x1, y1, x2, y2, gen + 1 )
+    }
+
+    recurse( x1, y1, x2, y2, gen ) {
+        let dist = ( x2 - x1 ) / 2
+
+        if ( dist >= 2 ) {
+            this.generate( x1, y1, x1 + dist, y1 + dist, gen )
+            this.generate( x1 + dist, y1, x2, y1 + dist, gen )
+            this.generate( x1, y1 + dist, x1 + dist, y2, gen )
+            this.generate( x1 + dist, y1 + dist, x2, y2, gen )
+        }
+    }
+
+    generateSquare( x1, y1, x2, y2, gen ) {
+        let size = ( x2 - x1 ) / 2
+        let mid = {
+            x: x1 + size,
+            y: y1 + size
+        }
+        let avg = this.getAverage( mid.x, mid.y, size )
+        // console.log( gen, mid.x, mid.y, ( avg + this.random( gen ) ).toFixed( 2 ) )
+        this.setCell( mid.x, mid.y, avg + this.random( gen ) )
+
+        // this.recurse( x1, y1, x2, y2, gen )
+    }
+
+
+    /*-----------------------------------------------------------*
+     *
+     *  Render methods
+     *
+     *-----------------------------------------------------------*/
+
+    lerpColor( value ) {
+        // Transform -1...1 to 0...1
+        let opacity = .5 + ( value * 0.5 )
+        return 'rgba( 255, 255, 255, ' + clamp({ num: opacity }) + ')'
     }
 
     clear() {
@@ -58,11 +133,16 @@ export default class DisplacementMap extends React.Component {
         let x1 = clamp({ num: x, max: this.props.gridSize - 1 })
         let y1 = clamp({ num: y, max: this.props.gridSize - 1 })
 
-        this.ctx.fillStyle = 'red'  // @TODO lerp grid value
+        this.ctx.fillStyle = this.lerpColor( cell )  // @TODO lerp grid value using cell param
+
+        if ( cell === 0 ) {
+            this.ctx.fillStyle = 'rgb(64,0,0)'
+        }
         this.ctx.fillRect( x1 * this.cellSize, y1 * this.cellSize, this.cellSize, this.cellSize )
     }
 
     renderGrid() {
+        this.clear()
         this.grid.forEach( ( row, x ) => {
             row.forEach( ( cell, y ) => {
                 this.drawCell( x, y, cell )
