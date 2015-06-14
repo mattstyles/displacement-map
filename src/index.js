074c81980992cb8b6ae370160023092305c3bbea
mattstyles <matt@veryfizzyjelly.com>
2015-06-14 20:57:24 +0100

    #add clamp and wrap

--- src/index.js
+++ src/index.js
@@ -2,8 +2,9 @@
 export default class DisplacementMap {
     constructor() {
         this.width = 65
+        this.height = 65
 
-        this.buffer = new ArrayBuffer( this.width * this.width )
+        this.buffer = new ArrayBuffer( this.width * this.height )
         this.array = new Uint8Array( this.buffer )
 
         this.generate()
@@ -14,16 +15,47 @@ export default class DisplacementMap {
         console.log( this.to1d( 2, 1 ) )
     }
 
+    /*-----------------------------------------------------------*
+     *
+     *  Generation methods
+     *
+     *-----------------------------------------------------------*/
+
+     generate() {
+         // No universal TypedArray.map so go old school
+         for ( let x = 0; x < this.array.length; x++ ) {
+             this.array[ x ] = ~~( Math.random() * 0xff )
+         }
+
+         return this.array
+     }
+
+
+    /*-----------------------------------------------------------*
+     *
+     *  Helpers
+     *
+     *-----------------------------------------------------------*/
+
+    /**
+     * Converts from 2d to 1d
+     */
     to1d( x, y ) {
         return ( y * this.width ) + x
     }
 
-    generate() {
-        // No universal TypedArray.map so go old school
-        for ( let x = 0; x < this.array.length; x++ ) {
-            this.array[ x ] = ~~( Math.random() * 0xff )
-        }
-
-        return this.array
+    /**
+     * Wraps 0...255 i.e. 257 becomes 1 (because 0 is important)
+     */
+    wrap( value ) {
+        return value & 0xff
     }
+
+    /**
+     * Clamps to 0...255
+     */
+    clamp( value ) {
+        return Math.max( Math.min( value, 255 ), 0 )
+    }
+
 }
