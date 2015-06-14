f323935d0696843ce5c5fb425cf697e2aaa2c632
mattstyles <matt@veryfizzyjelly.com>
2015-06-14 22:24:36 +0100

    #add square step

--- examples/src.js
+++ examples/src.js
@@ -1,12 +1,35 @@
 
 import Map from '../lib'
-let map = new Map()
+
+const WIDTH = 5
+const HEIGHT = 5
+const CELL_SIZE = 100
+
+let map = new Map({
+    width: WIDTH,
+    height: HEIGHT
+})
 
 window.map = map
 
 let canvas = document.createElement( 'canvas' )
 canvas.classList.add( 'Surface', 'js-surface' )
-canvas.setAttribute( 'width', 400 )
-canvas.setAttribute( 'height', 400 )
+canvas.setAttribute( 'width', WIDTH * CELL_SIZE )
+canvas.setAttribute( 'height', HEIGHT * CELL_SIZE )
+
+let ctx = canvas.getContext( '2d' )
+
+function lerp( value ) {
+    return 'rgba( 255, 255, 255, ' + ( value / 0xff ) + ' )'
+}
+
+
+for ( let x = 0; x < WIDTH; x++ ) {
+    for (let y = 0; y < HEIGHT; y++ ) {
+        ctx.fillStyle = lerp( map.get( x, y ) )
+        ctx.fillRect( x * CELL_SIZE, y * CELL_SIZE, CELL_SIZE, CELL_SIZE )
+    }
+}
+
 
 document.body.appendChild( canvas )
--- src/index.js
+++ src/index.js
@@ -1,18 +1,34 @@
 
 export default class DisplacementMap {
-    constructor() {
-        this.width = 65
-        this.height = 65
+    constructor( opts ) {
+        let options = opts || {}
+
+        // Assign defaults or use opts
+        // @TODO check power of 2 + 1 values
+        this.width = options.width || 65
+        this.height = options.height || 65
 
         this.buffer = new ArrayBuffer( this.width * this.height )
         this.array = new Uint8Array( this.buffer )
 
-        this.generate()
+        // Seed corners
+        this.array[ this.to1d( 0, 0 ) ] = 255
+        this.array[ this.to1d( 0, this.height - 1 ) ] = 255
+        this.array[ this.to1d( this.width - 1, 0 ) ] = 255
+        this.array[ this.to1d( this.width - 1, this.height - 1 ) ] = 255
+
+        this.generate( 1 )
 
         console.log( this.array )
 
-        console.log( this.to1d( 4, 0 ) )
-        console.log( this.to1d( 2, 1 ) )
+    }
+
+    get( x, y ) {
+        return this.array[ this.to1d( this.wrap( x ), this.wrap( y ) ) ]
+    }
+
+    set( x, y, value ) {
+        this.array[ this.to1d( this.wrap( x ), this.wrap( y ) ) ] = this.clamp( value )
     }
 
     /*-----------------------------------------------------------*
@@ -21,13 +37,68 @@ export default class DisplacementMap {
      *
      *-----------------------------------------------------------*/
 
-     generate() {
-         // No universal TypedArray.map so go old school
-         for ( let x = 0; x < this.array.length; x++ ) {
-             this.array[ x ] = ~~( Math.random() * 0xff )
-         }
+    getAvg() {
+        return Array.prototype.reduce.call( arguments, ( prev, num ) => prev + num ) / arguments.length
+    }
 
-         return this.array
+    /**
+     * Grabs the average of each edge point
+     */
+    getAvgEdge( x1, y1, x2, y2 ) {
+        let size = ~~( x2 - x1 ) / 2
+        return this.getAvg(
+            this.get( x1 + size, y1 ),
+            this.get( x1, y1 + size ),
+            this.get( x2, y1 + size ),
+            this.get( x1 + size, y2 )
+        )
+    }
+
+    /**
+     * Grabs the average from each corner point
+     */
+    getAvgCorner( x1, y1, x2, y2 ) {
+        return this.getAvg(
+            this.get( x1, y1 ),
+            this.get( x2, y1 ),
+            this.get( x1, y2 ),
+            this.get( x2, y2 )
+        )
+    }
+
+     /**
+      * @param cb <function> iterator to call
+      * @param step <float> 0...1
+      */
+     iterate( cb, step ) {
+         let size = ( this.width - 1 ) * step
+         for ( let x = 0; x < this.width - 1; x += size ) {
+             for ( let y = 0; y < this.height - 1; y += size ) {
+                 cb( x, y, x + size, y + size, step )
+             }
+         }
+     }
+
+     generate( step ) {
+         let dist = ( this.width - 1 ) * step
+
+         //  this.iterate( function( x1, y1, x2, y2 ) {
+         //      console.log( x1, y1, x2, y2 )
+         //      this.set( x1, y1, 127 )
+         //  }.bind( this ), step )
+         this.iterate( this.generateSquare, step )
+
+
+         if ( dist > 2 ) {
+             this.generate( step / 2 )
+         }
+     }
+
+     generateSquare = ( x1, y1, x2, y2, step ) => {
+         console.log( 'square', x1, y1, x2, y2 )
+         let mid = ~~( x2 - x1 ) / 2
+         let avg = this.getAvgCorner( x1, y1, x2, y2 )
+         this.set( x1 + mid, y1 + mid, avg )
      }
 
 
