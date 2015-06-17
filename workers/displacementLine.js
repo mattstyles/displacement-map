4824f8509c0e5075d296875ae89037ea47cfe4c4
Matt Styles <matt@veryfizzyjelly.com>
2015-06-17 08:52:27 +0100

    #add line midpoint displacement generation

--- workers/displacementLine.js
+++ workers/displacementLine.js
@@ -12,12 +12,32 @@
 var buf8
 
 
+function variance() {
+    return -0x80 + ( Math.random() * 0xff )
+}
+
+
+function generateMidpoint( start, end ) {
+    var middle = ( end - start ) / 2
+    var avg = ( buf8[ end ] + buf8[ start ] ) / 2
+
+    // Take average of 2 end points of segment
+    // * 1 is a smoothness/roughness variable
+    // variance decreases as segment length decreases
+    buf8[ middle ] = avg * 1 + ( variance() * ( end - start ) / buf8.length - 1 )
+}
+
 
 function generate( step ) {
     if ( !step ) {
         step = 1
     }
 
+    var size = ( buf8.length - 1 ) / step
+
+    for ( var i = 0; i < buf8.length - 1; i += size ) {
+        generateMidpoint( i, i + size )
+    }
 
     if ( size > 2 ) {
         generate( size * .5 )
